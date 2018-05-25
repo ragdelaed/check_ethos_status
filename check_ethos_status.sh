752121740c3f7450e065aeb08b722c7a8059c4ed
root <root@ragdelaed.com>
2018-05-25 07:56:26 -0400

    added throttle to conditional

--- check_ethos_status.sh
+++ check_ethos_status.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
 
-sites="6af48c dlirzc"
+sites="6af48c dlirzc00"
 
 for site in $(echo $sites)
 do
@@ -12,9 +12,10 @@ do
 		gpus=$(cat edtmp.json |jshon -e rigs -e $rig -e gpus|sed s'/"//g')
 		miner_instance=$(cat edtmp.json |jshon -e rigs -e $rig -e miner_instance|sed s'/"//g')
 		rack_loc=$(cat edtmp.json |jshon -e rigs -e $rig -e rack_loc|sed s'/"//g')
-		if [[ "$condition" != "mining" ]];
+		if [ "$condition" != "mining" ] && [ "$condition" != "throttle" ]
 		then
 			echo $site/$rack_loc,$miner_instance/$gpus/$condition|mail -s $site '6786306112@vtext.com'
+			#echo $site/$rack_loc,$miner_instance/$gpus/$condition
 			logger $site/$rack_loc,$miner_instance/$gpus/$condition
 		fi
 
